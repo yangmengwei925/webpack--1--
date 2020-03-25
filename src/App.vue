92e19bfa2fa558f8d5b389a0f76ebd1d95f72523
yangmengwei <1635433207@qq.com>
2020-03-26 00:12:09 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <h2>{{name}}</h2>
+
+    <button @click="add">+</button>
+    <h4>{{num}}</h4>
+    <button @click="sub">-</button>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        num: 0,
+        name: '蜡笔小新'
+      }
+    },
+    methods: {
+      add() {
+        this.num++;
+      },
+      sub() {
+        this.num--;
+      }
+
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
